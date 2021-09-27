74308e4c11be37faf27f65dfc0e8bbf595f4c553
Paul Vaneveld <paul@liters.nl>
2021-09-27 08:56:38 +0200

    select the previous day in the date filter

--- src/puppeteer/puppeteer.js
+++ src/puppeteer/puppeteer.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon');
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -11,22 +12,22 @@ const puppeteer = require('puppeteer');
 
     });
     const page = await browser.newPage();
-    await page.goto('https://www.wielerflits.nl/');
+    await page.goto('https://www.wielerflits.nl/nieuwsoverzicht/');
 
-    // cancel notifications
-
-
-    // await page.evaluate(() => {
-    //     document.querySelector('#onesignal-slidedown-cancel-button').click();
-    // });
-    const button = await page.waitForSelector('.align-right.secondary.slidedown-button');
-    button.click();
     // await gdpr iframe and accept all cookies
     const gdprFrameHandle = await page.waitForSelector('#gdpr-consent-notice');
     const frame = await gdprFrameHandle.contentFrame();
     await frame.click('#save');
 
 
-    // await frame.click('#save');
-    // await browser.close();
+    // open the date filter and select the previous day
+    await page.click('.btn.btn-warning');
+    await page.evaluate(() => {
+        const dateRangeInput = document.querySelector('#date_range');
+        dateRangeInput.removeAttribute('readonly');
+    });
+    const yesterday = DateTime.local().minus({ days: 1 }).toFormat('yyyy-MM-dd');
+    await page.type('#date_range', `${yesterday} to ${yesterday}`);
+    await page.keyboard.press('Escape');
+    await page.keyboard.press('Tab');
 })();
