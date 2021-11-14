07aa53c9c3912b9a3d91230ceafe0cb2302c38be
Paul Vaneveld <paul@liters.nl>
2021-11-14 12:09:10 +0100

    api updates

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,12 @@ import { ApolloServer } from 'apollo-server';
 import { PrismaClient } from '@prisma/client';
 import { GraphQLScalarType } from 'graphql';
 
-import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 Sentry.init({
-    dsn: "https://357348179eba40f0b38601dceab72210@o1041077.ingest.sentry.io/6010032",
-    integrations: [
-        new Tracing.Integrations.Postgres(),
-    ],
+    dsn: 'https://357348179eba40f0b38601dceab72210@o1041077.ingest.sentry.io/6010032',
+    integrations: [new Tracing.Integrations.Postgres()],
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
@@ -24,41 +22,40 @@ try {
 
     const resolvers = {
         Query: {
+            cyclist: async (parent, args, context) => {
+                const { id } = args;
+                if (id) {
+                    return await context.prisma.cyclist.findUnique({
+                        where: {
+                            id,
+                        },
+                    });
+                }
+            },
             cyclists: async (parent, args, context) => {
-                return context.prisma.cyclist.findMany({
+                return await context.prisma.cyclist.findMany({
                     include: {
                         articles: true,
-                    }
+                    },
                 });
             },
             articles: async (parent, args, context) => {
-                return context.prisma.article.findMany();
+                return await context.prisma.article.findMany({
+                    orderBy: args.orderBy,
+                    include: {
+                        cyclist: true,
+                    }
+                });
             },
-        },
-        Mutation: {
-            article: (parent, args, context) => create(context.prisma, 'article', {
-                published: args.published,
-                url: args.url,
-                medium: args.medium,
-                cyclistId: args.cyclistId,
-            }),
         },
         Date: new GraphQLScalarType({
             name: 'Date',
             description: 'A date and time, represented as an ISO-8601 string',
             serialize: (value) => value.toISOString(),
             parseValue: (value) => new Date(value),
-            parseLiteral: (ast: any) => new Date(ast.value)
+            parseLiteral: (ast: any) => new Date(ast.value),
         }),
-    }
-
-    const create = (prisma, serviceName, data) => {
-        const newItem = prisma[serviceName].create({
-            data
-        })
-        return newItem;
-    }
-
+    };
 
     const server = new ApolloServer({
         typeDefs: fs.readFileSync(
@@ -68,15 +65,13 @@ try {
         resolvers,
         context: {
             prisma,
-        }
-    })
+        },
+    });
 
     server
         .listen({ port: process.env.PORT || 4000 })
-        .then(({ url }) =>
-            console.log(`Server is running on ${url}`)
-        );
+        .then(({ url }) => console.log(`Server is running on ${url}`));
 } catch (error) {
+    console.log(error);
     Sentry.captureException(error);
 }
-
